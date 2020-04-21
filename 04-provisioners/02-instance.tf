db80e2a47cdba19c8b8d136bfc4143d721cd37ba
BavisettyMahesh <bavisetty.veera@gmail.com>
2020-04-21 13:26:52 +0530

    [RS-3] create an instance and configure robot-shop project with remote-exec provisioners.

---
+++ 04-provisioners/02-instance.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "sample" {
+  ami = "ami-004cd5eb616d96667"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-080e9bce8cef41ff1"]
+
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "root"
+      password = "DevOps321" // need to remove passwords later while we talk about password management.
+      host     = self.public_ip
+    }
+    inline = [
+      "cd /root",
+      "git clone https://bvvmahesh:Mahesh@0524@github.com/bvvmahesh/Shell-Scripting.git",
+      "sh Shell-Scripting/robot-shop/project.sh"
+    ]
+  }
+}
