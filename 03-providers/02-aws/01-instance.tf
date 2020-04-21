20d8d166bdf7bc82377503a079a4c222a8e88941
BavisettyMahesh <bavisetty.veera@gmail.com>
2020-04-21 11:36:46 +0530

    [RS-2] updating ec2 instance type.

--- 03-providers/02-aws/01-instance.tf
+++ 03-providers/02-aws/01-instance.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 resource "aws_instance" "sample" {
   ami = "ami-004cd5eb616d96667"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-080e9bce8cef41ff1"]
 }
