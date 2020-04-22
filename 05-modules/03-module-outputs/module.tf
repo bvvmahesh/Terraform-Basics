ccb976ec6d462e0fe67ddbbf906e9f4da078bdeb
BavisettyMahesh <bavisetty.veera@gmail.com>
2020-04-22 11:34:55 +0530

    creating security group module

--- 05-modules/03-module-outputs/module.tf
+++ 05-modules/03-module-outputs/module.tf
@@ -2,7 +2,7 @@ module "security-group" {
   source = "./security-group"
 }
 
-module "ec2" {
+module "ec2-server" {
   source = "./ec2-server"
   SECURITY_GROUP = module.security-group.SECURITY_GROUP
 }
