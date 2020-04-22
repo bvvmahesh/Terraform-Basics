26f2b24ba8feca8c403949e7972f3e713f2fe7d3
BavisettyMahesh <bavisetty.veera@gmail.com>
2020-04-22 13:07:25 +0530

    creating functions

--- 06-functions/functions.tf
+++ 06-functions/functions.tf
@@ -10,3 +10,9 @@ variable "string_to_list" {}
 output "string_to_list" {
   value = split(",", var.string_to_list)
 }
+
+variable "lower_case_to_upper_case" {}
+
+output "upper_case" {
+  value = upper(var.lower_case_to_upper_case)
+}
