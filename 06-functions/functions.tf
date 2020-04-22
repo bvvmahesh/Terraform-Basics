a304705502439553b118bbe4bd666f54c86450d9
BavisettyMahesh <bavisetty.veera@gmail.com>
2020-04-22 12:58:35 +0530

    creating functions

--- 06-functions/functions.tf
+++ 06-functions/functions.tf
@@ -3,3 +3,10 @@ variable "Remove_spaces" {}
 output "Removed_Spaces" {
   value = replace(var.Remove_spaces," " , "")
 }
+
+
+variable "string_to_list" {}
+
+output "string_to_list" {
+  value = split(",", var.string_to_list)
+}
