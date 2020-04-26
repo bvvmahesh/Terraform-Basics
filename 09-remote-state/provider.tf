891f1c2a1c6d104b6544afe18ccbf2b7ac19622e
BavisettyMahesh <bavisetty.veera@gmail.com>
2020-04-26 18:14:12 +0530

    creating functions

--- 09-remote-state/provider.tf
+++ 09-remote-state/provider.tf
@@ -11,27 +11,9 @@ terraform {
   }
 }
 
-resource "aws_security_group" "allow-ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_ssh"
-  }
+resource "aws_security_group" "allow-sample" {
+  name        = "sample"
+  description = "sample"
 }
 
 resource "null_resource" "none" {
